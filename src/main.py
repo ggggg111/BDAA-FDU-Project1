43c5f7a6f1db0c44e72a6a9c2e8b4132d529c17b
nngg1104 <nngg1104@gmail.com>
2021-12-27 00:02:43 +0100

    Loading dataset.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 import nltk
+import json
+import numpy as np
 
 def main():
-    sentence = "hello world"
-    tokens = nltk.word_tokenize(sentence)
-    print(tokens)
+    with open("workdir/clinc150_uci/data_full.json") as f:
+        data = json.load(f)
+
+    train_X, train_y = map( list, zip(*data["train"]) )
+    test_X, test_y = map( list, zip(*data["test"]) )
+
+    print( len(train_X) )
+    print( len(train_y) )
+
+    print( len(test_X) )
+    print( len(test_y) )
 
 if __name__ == "__main__":
     main()
