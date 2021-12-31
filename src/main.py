2daf87e385f7d4f0e8d54ca4a6c766c4d29a33b7
nngg1104 <nngg1104@gmail.com>
2021-12-31 20:24:17 +0100

    SVM predictions on the text data.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
+import numpy as np
 import string
 import json
 
-from nltk import ngrams
 from nltk.tokenize import word_tokenize
 
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.svm import SVC
+
+class Metrics:
+    @staticmethod
+    def accuracy(predictions, ground_truth):
+        assert len(predictions) == len(ground_truth)
+        return np.mean(predictions == ground_truth)
 
 def preprocessing(data):
     preprocessed_data = []
@@ -23,8 +30,7 @@ def feature_matrix(data):
     matrix = CountVectorizer()
     matrix_fit = matrix.fit_transform(data)
     features = matrix.get_feature_names()
-    
-    print(f"Feature names: {features}")
+
     print(f"Feature length: {len(features)}")
 
     return matrix_fit.toarray()
@@ -33,14 +39,23 @@ def main():
     with open("workdir/clinc150_uci/data_full.json") as file:
         data = json.load(file)
 
-    train_X_data, train_y_data = map( list, zip(*data["train"]) )
-    test_X_data, test_y_data = map( list, zip(*data["test"]) )
+    train_X_data, train_y = map( list, zip(*data["train"]) )
+    test_X_data, test_y = map( list, zip(*data["test"]) )
 
-    train_X_preprocessed = preprocessing(train_X_data)
-    test_X_preprocessed = preprocessing(test_X_data)
-    
-    train_X = feature_matrix(train_X_data)
-    test_X = feature_matrix(test_X_data)
+    train_X_n = np.shape(train_X_data)[0]
+    test_X_n = np.shape(test_X_data)[0]
+
+    data = np.concatenate( (train_X_data, test_X_data), axis=0 )
+    data_preprocessed = preprocessing(data)
+    data_matrix = feature_matrix(data_preprocessed)
+
+    test_X = data_matrix[train_X_n:]
+    train_X = np.delete(data_matrix, np.s_[train_X_n:], 0)
+
+    svc = SVC(kernel="linear", C=100).fit(train_X, train_y)
+    predictions = svc.predict(test_X)
+    accuracy = Metrics.accuracy(predictions, test_y)
+    print(f"Accuracy: {accuracy}")
 
 if __name__ == "__main__":
     main()
