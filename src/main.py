283ba8fecc3e2029e0c2ef2e2b2cfd8a10d36768
nngg1104 <nngg1104@gmail.com>
2021-12-31 15:25:05 +0100

    Constructed feature matrices.

--- src/main.py
+++ src/main.py
@@ -8,15 +8,21 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 def preprocessing(data):
     preprocessed_data = []
-    
+
     for element in data:
         element = element.lower()
         element = "".join([char for char in element if char not in string.punctuation])
         tokenized_element = word_tokenize(element)
-        
-        preprocessed_data.append(tokenized_element)
-        
+        # Add more preprocessing for each word if necessary
+
+        preprocessed_data.append( " ".join(tokenized_element) )
+
     return preprocessed_data
+
+def feature_matrix(data):
+    matrix = CountVectorizer(max_features=1000)
+
+    return matrix.fit_transform(data).toarray()
 
 def main():
     with open("workdir/clinc150_uci/data_full.json") as file:
@@ -24,9 +30,12 @@ def main():
 
     train_X_data, train_y_data = map( list, zip(*data["train"]) )
     test_X_data, test_y_data = map( list, zip(*data["test"]) )
+
+    train_X_preprocessed = preprocessing(train_X_data)
+    test_X_preprocessed = preprocessing(test_X_data)
     
-    train_X = preprocessing(train_X_data)
-    test_X = preprocessing(test_X_data)
+    train_X = feature_matrix(train_X_data)
+    test_X = feature_matrix(test_X_data)
 
 if __name__ == "__main__":
     main()
