fbda11296ac38c7b662a7aab416c55e39c18d2f1
nngg1104 <nngg1104@gmail.com>
2021-12-31 13:45:52 +0100

    Data tokenization and basic preprocessing, having all words still.

--- src/main.py
+++ src/main.py
@@ -1,19 +1,32 @@
-import nltk
+import string
 import json
-import numpy as np
+
+from nltk import ngrams
+from nltk.tokenize import word_tokenize
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+def preprocessing(data):
+    preprocessed_data = []
+    
+    for element in data:
+        element = element.lower()
+        element = "".join([char for char in element if char not in string.punctuation])
+        tokenized_element = word_tokenize(element)
+        
+        preprocessed_data.append(tokenized_element)
+        
+    return preprocessed_data
 
 def main():
-    with open("workdir/clinc150_uci/data_full.json") as f:
-        data = json.load(f)
+    with open("workdir/clinc150_uci/data_full.json") as file:
+        data = json.load(file)
 
-    train_X, train_y = map( list, zip(*data["train"]) )
-    test_X, test_y = map( list, zip(*data["test"]) )
-
-    print( len(train_X) )
-    print( len(train_y) )
-
-    print( len(test_X) )
-    print( len(test_y) )
+    train_X_data, train_y_data = map( list, zip(*data["train"]) )
+    test_X_data, test_y_data = map( list, zip(*data["test"]) )
+    
+    train_X = preprocessing(train_X_data)
+    test_X = preprocessing(test_X_data)
 
 if __name__ == "__main__":
     main()
